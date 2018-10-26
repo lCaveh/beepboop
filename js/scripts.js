20a5ec6fcfbd55f0dd835566fd1b65f22aa9f835
Kaveh <lcaveh@gmail.com>
2018-10-26 09:25:49 -0700

    Main logic done

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
     var name = $("#name").val();
     var number = $("#number").val();
-    var initialArray = []
+    var BeepArray = []
     for (var i=0;i<=number;i++){
-      initialArray[i]= i;
+      BeepArray[i]= i.toString();
+      if (i>0 && i%3==0) {
+        BeepArray[i] = "I'm sorry, "+name+". I'm afraid I can't do that."
+      };
+      if (BeepArray[i].match(/1/)) {
+        BeepArray[i] = "Boop!"
+      };
+      if (BeepArray[i].match(/0/)) {
+        BeepArray[i] = "Beep!"
+      };
     };
-
-
-
-
   });
 });
